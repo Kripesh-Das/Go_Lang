b0b43c679cd4b39b1a477f2e345fa6da821a3db8
Kripesh <kripeshdas2jio@gmail.com>
2025-06-19 12:51:26 +0530

    small changes

---
+++ 19_goroutines/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main(){
+	go greeter("Hello")
+	greeter("World")
+}
+
+func greeter(s string){
+
+	for i:=0; i<5; i++ {
+		time.Sleep(3 * time.Microsecond)
+		fmt.Println(s)
+	}
+
+}
